0afebfd79c9ae7190ff177ec82adbdaec76968e0
Charles Prestia <cjprestia@gmail.com>
2016-11-01 15:42:48 -0400

    adjustments - css relative url was incorrect in main.handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,20 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'root',
-	password: 'Fthefed22',
-	database: 'burgers_db'
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Fthefed22',
+    database: 'burgers_db'
+    });
+};
+
+connection.connect(function(err) {
+	if (err) throw err;
+	console.log('Connected as ID '+connection.threadId);
 });
 
 module.exports = connection;
